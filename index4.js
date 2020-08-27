0007ffed4ada469fe32b872290101a97057392f8
zzanghit <68940997+zzanghit@users.noreply.github.com>
2020-08-28 00:36:09 +0900

    Add files via upload

--- index4.js
+++ index4.js
@@ -4,8 +4,8 @@ const logs = document.querySelector('#logs');
 
 let count = 0;
 
-var answer = [];
-var pickNumbers = 4;
+let answer = [];
+const pickNumbers = 4;
                  
 for(insertCur = 0; insertCur < pickNumbers ; insertCur++){
     answer[insertCur] = Math.floor(Math.random() * 10);
@@ -22,8 +22,32 @@ console.log(answer);
 
 check.addEventListener('click', () => {
     const value = input.value;
-    if(value && value.length === 4) {
-
+    if(count > 9) {
+        logs.appendChild(document.createTextNode('Game Over! ' + 'Answer = ' + answer.join('')));
+        logs.appendChild(document.createElement('br'));
+    } else if(value && value.length === 4) {
+        count++;
+        if(answer.join('') === value) { // answer[] 을 string 으로 변환해서 비교
+            logs.appendChild(document.createTextNode('HR'));
+        } else {
+            let strike = 0;
+            let ball = 0;
+            for(const [aIndex, aNumber] of answer.entries()) {
+                for(const [bIndex, bString] of input.value.split('').entries()) {
+                    if(aNumber === Number(bString)) {
+                        if(aIndex === bIndex) {
+                            strike++;
+                        } else {
+                            ball++;
+                        }
+                    }
+                }
+            }
+            // logs.appendChild(document.createTextNode(`${strike} strike ${ball} ball`)); 같은 표현 방식
+            logs.appendChild(document.createTextNode(input.value + ' : ' +strike + ' strike ' + ball + ' ball'));
+            logs.appendChild(document.createElement('br'));
+        }
     }
+    input.value = '';
 });
 
